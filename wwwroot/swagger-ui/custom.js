a99b965de9436c384cc019bb89bb30667460137f
Mack <macknz7@gmail.com>
2020-12-16 13:36:33 -0800

    index.html ui update

---
+++ wwwroot/swagger-ui/custom.js
@@ -0,0 +1,12 @@
+﻿var a = document.querySelector('head > link:nth-child(4)');
+if (a) {
+    a.setAttribute('href', 'favicon.ico')
+}
+var b = document.querySelector('head > link:nth-child(5)');
+if (b) {
+    b.setAttribute('href', 'favicon.ico')
+}
+
+var c = document.querySelector('#swagger-ui > section > div.topbar > div > div > a');
+var t = "Rock Api";
+c.appendChild(t);
